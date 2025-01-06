4ade98ef5766dcf0d1112203fd1d6ab0f2c92437
dbednorz@tuta.io <dbednorz@tuta.io>
2025-01-06 20:22:09 +0100

    Added file options  and specified the window size

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <QApplication>
 #include <QHBoxLayout>
 #include <QTextEdit>
+#include <QMenuBar>
+#include <QAction>
 
 int main(int argc, char **argv){
     QApplication app (argc, argv);
@@ -11,15 +13,27 @@ int main(int argc, char **argv){
     QMainWindow mainWindow;
     QWidget centralWidget;
     QHBoxLayout layout;
+    QMenuBar *menuBar = mainWindow.menuBar();
+    QMenu *fileMenu = menuBar->addMenu("File");
+    QAction fileSave;
 
     QTextEdit textEdit;
 
     layout.addWidget(&textEdit);
 
+    fileMenu->addAction("Save");
+    fileMenu->addAction("Save as");
+    fileMenu->addAction("Open");
+    fileMenu->addAction("Exit");
+
+
     centralWidget.setLayout(&layout);
     mainWindow.setCentralWidget(&centralWidget);
+    mainWindow.resize(800, 800);
 
-    //textEdit.display();
+
+
+
     mainWindow.show();
     return app.exec();
 }
