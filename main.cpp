e7a410302515d25b5c9ba1ccdb9291a4743f4619
dbednorz@tuta.io <dbednorz@tuta.io>
2025-01-08 19:22:41 +0100

    Added blueprint for menubar buttons functionality

--- main.cpp
+++ main.cpp
@@ -6,6 +6,11 @@
 #include <QTextEdit>
 #include <QMenuBar>
 #include <QAction>
+#include <QMessageBox>
+#include <QString>
+#include <fstream>
+
+using namespace std;
 
 int main(int argc, char **argv){
     QApplication app (argc, argv);
@@ -15,21 +20,47 @@ int main(int argc, char **argv){
     QHBoxLayout layout;
     QMenuBar *menuBar = mainWindow.menuBar();
     QMenu *fileMenu = menuBar->addMenu("File");
-    QAction fileSave;
 
     QTextEdit textEdit;
 
     layout.addWidget(&textEdit);
 
-    fileMenu->addAction("Save");
-    fileMenu->addAction("Save as");
-    fileMenu->addAction("Open");
-    fileMenu->addAction("Exit");
+
+    QAction *saveAction = fileMenu->addAction("Save");
+    QAction *saveAsAction = fileMenu->addAction("Save as");
+    QAction *openAction = fileMenu->addAction("Open");
+    QAction *exitAction = fileMenu->addAction("Exit");
 
 
     centralWidget.setLayout(&layout);
     mainWindow.setCentralWidget(&centralWidget);
     mainWindow.resize(800, 800);
+
+    QObject::connect(saveAction, &QAction::triggered, [&]() {
+        QMessageBox fileSavedSucces;
+        QString savedText = textEdit.toPlainText();
+        std::fstream file("example.txt");
+        //file << savedText;
+        fileSavedSucces.setText("File saved!");
+        fileSavedSucces.exec();
+    });
+
+    QObject::connect(saveAsAction, &QAction::triggered, [&]() {
+        QMessageBox fileSavedSucces;
+        fileSavedSucces.setText("File saved!");
+        fileSavedSucces.exec();
+    });
+
+    QObject::connect(openAction, &QAction::triggered, [&]() {
+
+
+    });
+
+    QObject::connect(exitAction, &QAction::triggered, [&]() {
+
+
+    });
+
 
 
 
